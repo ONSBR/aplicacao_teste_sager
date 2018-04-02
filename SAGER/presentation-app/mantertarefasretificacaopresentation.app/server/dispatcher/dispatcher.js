a62c3a667848121129dd2fe3e82e62a89eeefd44
Philippe Moneda <philippemoneda@gmail.com>
2018-04-02 20:53:26 -0300

    ajuste

--- SAGER/presentation-app/mantertarefasretificacaopresentation.app/server/dispatcher/dispatcher.js
+++ SAGER/presentation-app/mantertarefasretificacaopresentation.app/server/dispatcher/dispatcher.js
@@ -12,7 +12,6 @@ dispatcher.register("presentation.insere.tarefa.request", (scope, ok, error)=>{
 
 dispatcher.register("presentation.atualiza.tarefa.request", (scope, ok, error)=>{
     scope.bind(scope.params.tarefa);
-    var a = 1;
     scope.context.save().then(ok).catch(error);
 
 });
