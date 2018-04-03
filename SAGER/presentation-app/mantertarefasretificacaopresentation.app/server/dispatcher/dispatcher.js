5bc8bb928a9cbc9bf44787d13b16cb2c25d06d23
Philippe Moneda <philippemoneda@gmail.com>
2018-04-03 17:35:37 -0300

    ajustes

--- SAGER/presentation-app/mantertarefasretificacaopresentation.app/server/dispatcher/dispatcher.js
+++ SAGER/presentation-app/mantertarefasretificacaopresentation.app/server/dispatcher/dispatcher.js
@@ -3,10 +3,11 @@ const TarefaDAO = new (require("../dao/tarefadao"))();
 
 const dispatcher = new (require('plataforma-sdk/worker/dispatcher'))();
 
+const TarefaRetificacao = require('../domain/TarefaRetificacao');
 
 
 dispatcher.register("presentation.insere.tarefa.request", (context, resolve, reject)=>{
-    var entity = TarefaDAO.createTarefaRetificacaoArgs(context.event.payload.nomeTarefa);
+    var entity = new TarefaRetificacao(context.event.payload.nomeTarefa);
     context.dataset.tarefaretificacao.insert(entity);
     resolve(entity);
 });
