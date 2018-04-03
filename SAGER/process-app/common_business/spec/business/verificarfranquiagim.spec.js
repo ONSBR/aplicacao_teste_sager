87a5abdc2cb1f7af83349f3cb563ca5e1518c138
Hugo Menezes <hugomenezes@gmail.com>
2018-04-03 15:08:53 -0300

    RNI 217

--- SAGER/process-app/common_business/spec/business/verificarfranquiagim.spec.js
+++ SAGER/process-app/common_business/spec/business/verificarfranquiagim.spec.js
@@ -48,7 +48,14 @@ describe('EventoMudancaEstadoOperativoBusiness deve:', function () {
                 }
             ).toThrowError('Não pode haver registro de evento com Origem “GIM” que ultrapasse o limite de 12 meses, contados a partir de 120 meses da data de entrada em operação comercial.');
 
+            let eventosQueNaoExcedem12Meses = [
+                { idEstadoOperativo: 'EOC', idClassificacaoOrigem: 'GUM', potenciaDisponivel: 500, dataVerificada: new Date(2014, 10, 1) },
+                { idEstadoOperativo: 'LIG', idClassificacaoOrigem: 'GUM', potenciaDisponivel: 500, dataVerificada: new Date(2014, 10, 1, 0, 0, 0) },
+                { idEstadoOperativo: 'LIG', idClassificacaoOrigem: 'GIM', potenciaDisponivel: 500, dataVerificada: new Date(2024, 10, 2, 0, 0, 0) },
+                { idEstadoOperativo: 'LIG', idClassificacaoOrigem: 'GIM', potenciaDisponivel: 500, dataVerificada: new Date(2024, 10, 4, 0, 0, 0) }
+            ];
 
+            eventoMudancaEstadoOperativoBusiness.verificarRestricaoTempoUtilizacaoFranquiaGIM(eventosQueNaoExcedem12Meses);
         });
 
 });
