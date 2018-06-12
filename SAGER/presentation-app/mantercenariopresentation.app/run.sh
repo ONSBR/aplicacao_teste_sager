50e8cb5e6683d70a7b9770a72f4aab7635e81e69
hugomenezes <hugomenezes@gmail.com>
2018-06-12 17:53:26 -0300

    ajustes no script

--- SAGER/presentation-app/mantercenariopresentation.app/run.sh
+++ SAGER/presentation-app/mantercenariopresentation.app/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo 'Iniciando build da aplicação web'
-npm i manter-cenario-web/
 cd manter-cenario-web/
+npm install
 ng build
 echo 'Build da aplicação web finalizado'
 echo 'Cópia da dist web'
-cp dist/* ../server/dist/
+cp -rf dist/* ../server/dist/
 echo 'Cópia finalizada'
 cd ..
 echo 'Iniciando deploy da app'
