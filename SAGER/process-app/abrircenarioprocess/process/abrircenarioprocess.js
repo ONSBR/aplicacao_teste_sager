54eecdc5bda4d3763113785f3d093ae863d303da
hugomenezes <hugomenezes@gmail.com>
2018-07-05 11:00:46 -0300

    aplicando regras

--- SAGER/process-app/abrircenarioprocess/process/abrircenarioprocess.js
+++ SAGER/process-app/abrircenarioprocess/process/abrircenarioprocess.js
@@ -11,14 +11,32 @@ SDK.run((context, resolve, reject, fork) => {
     try {
         let eventoBusiness = new EventosBusiness();
         let cenario = new Cenario();
-        cenario.abrir(context.event.payload, fork);    
+        cenario.abrir(context.event.payload, fork);
         cenario.aplicarCriterios(context);
+
+
         let eventos = context.dataset.eventomudancaestadooperativo.collection.toArray();
-        eventoBusiness.aplicarRegrasCenario(eventos);
-        eventos.filter(eventoToDelete => {
-            return eventoToDelete.operacao != undefined && eventoToDelete.operacao == 'E';
-        }).forEach(eventoToDelete => {
-            dataset.eventomudancaestadooperativo.delete(eventoToDelete);
+
+        let uges = new Set(eventos.map(evento => evento.idUge));
+
+        uges.forEach(idUge => {
+            let eventosByUge = eventos.filter(eventoByUge => {
+                return eventoByUge.idUge == idUge;
+            });
+            eventosByUge.sort(function (a, b) {
+                if (a.dataVerificada < b.dataVerificada)
+                    return -1;
+                if (a.dataVerificada > b.dataVerificada)
+                    return 1;
+                return 0;
+            });
+            eventoBusiness.aplicarRegrasCenario(eventosByUge);
+            eventosByUge.filter(eventoToDelete => {
+                return eventoToDelete.operacao != undefined && eventoToDelete.operacao == 'E';
+            }).forEach(eventoToDelete => {
+                // console.log('Id evento para exclusão=' + eventoToDelete.idEvento);
+                context.dataset.eventomudancaestadooperativo.delete(eventoToDelete);
+            });
         });
         resolve();
     } catch (error) {
