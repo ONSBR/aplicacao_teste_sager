f823010e66633add448b7a305186b3e75ffd20e0
Fernando Cavalcante de Andrade <fca@intranet.pitang.com>
2018-08-10 18:51:20 -0300

    Alteração de deploy

--- SAGER/deploy/deploy_all.sh
+++ SAGER/deploy/deploy_all.sh
@@ -5,14 +5,15 @@ plataforma --deploy local
 
 echo 'Deploy Presentation'
 cd ../../presentation-app/portal/
+npm install
 plataforma --deploy local
 
 cd ../consultarhistoricotaxaspresentation.app/
 cd server
-rm -rf node_modules
+npm uninstall
 npm install
 cd ../consultar-historico-taxas-web
-rm -rf node_modules
+npm uninstall
 cd ..
 ./run.sh
 
@@ -20,35 +21,35 @@ node server/spec/script/load_data_test.js
 
 cd ../mantercenariopresentation.app/
 cd server
-rm -rf node_modules
+npm uninstall
 npm install
 cd ../manter-cenario-web
-rm -rf node_modules
+npm uninstall
 cd ..
 ./run.sh
 
 cd ../mantertarefasretificacaopresentation.app/
 cd server
-rm -rf node_modules
+npm uninstall
 npm install
 cd ../mantertarefasretificacao-web
-rm -rf node_modules
+npm uninstall
 cd ..
 ./run.sh
 
 echo 'Deploy Process'
 cd ../../process-app/abrircenarioprocess/
-rm -rf node_modules
+npm uninstall
 npm install
 plataforma --deploy local
 
 cd ../executarcalculotaxas/
-rm -rf node_modules
+npm uninstall
 npm install
 plataforma --deploy local
 
 cd ../calculartaxasprocess/
-rm -rf node_modules
+npm uninstall
 npm install
 plataforma --deploy local
 
